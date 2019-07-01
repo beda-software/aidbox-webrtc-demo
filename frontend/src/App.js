54edbcc52bb4f23b7c8ded99ae75e52d4e2c2fb7
jussiemion <jussiemion@yandex.ru>
2019-07-01 13:00:04 +0700

    Display local stream

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
+import  { Loader, Button, Icon } from 'semantic-ui-react';
+
 import './App.css';
 
 const App = () => {
   const socket = io('http://localhost:3001');
 
+  const [receivedUserMedia, setReceivedUserMedia] = useState(false);
+  const [localStream, setLocalStream] = useState(null);
+
+  useEffect(() => {
+    if (!receivedUserMedia) {
+      navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      }).then((localStream) => {
+        setReceivedUserMedia(true);
+        setLocalStream(localStream);
+      })
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="App-chat">
+        <div className="App-chat-main">
+          {localStream ? (
+            <video
+              ref={(video) => video.srcObject = localStream}
+              autoPlay
+            />
+          ) : (
+            <Loader active />
+          )}
+        </div>
+        <div className="App-chat-controls">
+          <Button
+            content="Пригласить в чат"
+            labelPosition="left"
+            color="green"
+            icon="share alternate"
+          />
+        </div>
+      </div>
     </div>
   )
 };
